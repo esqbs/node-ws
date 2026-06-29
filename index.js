5f78e1ac69ffa9d76893417b151da756c16efca0
ಠ_ಠ <63218201+esqbs@users.noreply.github.com>
2026-06-29 12:03:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const httpServer = http.createServer((req, res) => {
     return;
   } else if (req.url === `/${SUB_PATH}`) {
     const namePart = NAME ? `${NAME}-${ISP}` : ISP;
-    const vlessURL = `vless://` + encodeURIComponent(`${UUID}@${DOMAIN}:443?encryption=none&security=tls&sni=${DOMAIN}&fp=chrome&type=ws&host=${DOMAIN}&path=/${WSPATH}#${namePart}`);
+    const vlessURL = `vless://${UUID}@${DOMAIN}:443?encryption=none&security=tls&sni=${DOMAIN}&fp=chrome&type=ws&host=${DOMAIN}&path=%2F${WSPATH}#${namePart}`);
     const trojanURL = `trojan://${UUID}@${DOMAIN}:443?security=tls&sni=${DOMAIN}&fp=chrome&type=ws&host=${DOMAIN}&path=%2F${WSPATH}#${namePart}`;
     const subscription = vlessURL + '\n' + trojanURL;
     // const base64Content = Buffer.from(subscription).toString('base64');
